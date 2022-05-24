1b22a9a9423dbe640f96149faa390b0d9c19ed99
murilomontino <murilomontinojr@hotmail.com>
2022-05-24 17:59:28 -0300

    [feat] tratamento de erros dinamico

---
+++ source/shared/either.js
@@ -0,0 +1,37 @@
+class Left {
+	constructor(value) {
+		this.value = value
+	}
+	isLeft() {
+		return true
+	}
+
+	isRight() {
+		return false
+	}
+}
+
+class Right {
+	constructor(value) {
+		this.value = value
+	}
+	isLeft() {
+		return false
+	}
+
+	isRight() {
+		return true
+	}
+}
+
+class Either {
+	static Left(value) {
+		return new Left(value)
+	}
+
+	static Right(value) {
+		return new Right(value)
+	}
+}
+
+module.exports = Either
